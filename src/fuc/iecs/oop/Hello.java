cfbdf2ecd59bf6f65254521930c792db8fa7fe3c
su83032312 <su83032312@gmail.com>
2016-10-05 13:57:35 +0900

    作業

--- src/fuc/iecs/oop/Hello.java
+++ src/fuc/iecs/oop/Hello.java
@@ -3,7 +3,10 @@ package fuc.iecs.oop;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello OOP");
+		String lyrics="Let it go!Let it go!Cannot hold it back anymore";
+		lyrics=lyrics.replace("it", "her");
+		lyrics=lyrics.replace("Cannot", "Can't");
+		System.out.println(lyrics);
 
 	}
 
